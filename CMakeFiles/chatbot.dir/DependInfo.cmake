6535a688b1ee4a80aa47af7c249c1b5bc3ed5a48
Rand Asswad <asswad.rand@gmail.com>
2018-05-24 18:44:20 +0200

    Makefile

---
+++ CMakeFiles/chatbot.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/Agent/Agent.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/Agent/Agent.cpp.o"
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/Agent/ELIZA/Decomp.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/Agent/ELIZA/Decomp.cpp.o"
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/Agent/ELIZA/Eliza.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/Agent/ELIZA/Eliza.cpp.o"
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/Agent/ELIZA/Key.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/Agent/ELIZA/Key.cpp.o"
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/Agent/ELIZA/Mapper.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/Agent/ELIZA/Mapper.cpp.o"
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/Agent/ELIZA/Memory.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/Agent/ELIZA/Memory.cpp.o"
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/Agent/ELIZA/Reasmb.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/Agent/ELIZA/Reasmb.cpp.o"
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/Agent/ELIZA/ScriptParser.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/Agent/ELIZA/ScriptParser.cpp.o"
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/Agent/ELIZA/Synonyms.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/Agent/ELIZA/Synonyms.cpp.o"
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/Agent/ELIZA/Thesaurus.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/Agent/ELIZA/Thesaurus.cpp.o"
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/Agent/Parser.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/Agent/Parser.cpp.o"
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/Client/Console/TerminalClient.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/Client/Console/TerminalClient.cpp.o"
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/Client/Console/main.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/Client/Console/main.cpp.o"
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/Client/GUI/GUIclient.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/Client/GUI/GUIclient.cpp.o"
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/Client/Web/WebClient.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/Client/Web/WebClient.cpp.o"
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/ns_test.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/ns_test.cpp.o"
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/utils.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/utils.cpp.o"
+  "/data/dev/c++/ELIZA_PROJECT/chatbot/src/utils/String.cpp" "/data/dev/c++/ELIZA_PROJECT/chatbot/CMakeFiles/chatbot.dir/src/utils/String.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/chatbot.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chatbot.dir/src/Agent/ELIZA/Decomp.cpp.o"
+  "CMakeFiles/chatbot.dir/src/Agent/ELIZA/Eliza.cpp.o"
+  "CMakeFiles/chatbot.dir/src/Agent/ELIZA/Key.cpp.o"
+  "CMakeFiles/chatbot.dir/src/Agent/ELIZA/Mapper.cpp.o"
+  "CMakeFiles/chatbot.dir/src/Agent/ELIZA/Memory.cpp.o"
+  "CMakeFiles/chatbot.dir/src/Agent/ELIZA/ScriptParser.cpp.o"
+  "CMakeFiles/chatbot.dir/src/Agent/ELIZA/Synonyms.cpp.o"
+  "CMakeFiles/chatbot.dir/src/Agent/ELIZA/Thesaurus.cpp.o"
+  "CMakeFiles/chatbot.dir/src/Agent/Agent.cpp.o"
+  "CMakeFiles/chatbot.dir/src/Agent/Parser.cpp.o"
+  "CMakeFiles/chatbot.dir/src/Client/Console/TerminalClient.cpp.o"
+  "CMakeFiles/chatbot.dir/src/Client/GUI/GUIclient.cpp.o"
+  "CMakeFiles/chatbot.dir/src/Client/Web/WebClient.cpp.o"
+  "CMakeFiles/chatbot.dir/src/Client/Console/main.cpp.o"
+  "CMakeFiles/chatbot.dir/src/utils.cpp.o"
+  "CMakeFiles/chatbot.dir/src/ns_test.cpp.o"
+  "CMakeFiles/chatbot.dir/src/utils/String.cpp.o"
+  "CMakeFiles/chatbot.dir/src/Agent/ELIZA/Reasmb.cpp.o"
+  "chatbot.pdb"
+  "chatbot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chatbot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
